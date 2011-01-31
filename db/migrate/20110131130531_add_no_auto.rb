2b1ef098c0da0dc239ac750c0b05cc9d836c7f0f
unknown <SaloO@.(none)>
2011-01-31 18:13:20 +0500

    nothing

---
+++ db/migrate/20110131130531_add_no_auto.rb
@@ -0,0 +1,14 @@
+class AddNoAuto < ActiveRecord::Migration
+  def self.up
+     create_table(:packages, :id => false) do |t|
+      t.integer :id, :options => 'PRIMARY KEY'
+      t.float  "hours"
+      t.string "package_type"
+      t.float  "price"
+    end
+  end
+
+  def self.down
+    drop_table :groups
+  end
+end
