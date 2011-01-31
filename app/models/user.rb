753fcc15e1e1e9705336eb3d1fabb21b75227b21
unknown <SaloO@.(none)>
2011-01-31 19:11:09 +0500

    nothing

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ActiveRecord::Base
   #validates_uniqueness_of   :login
   validates_format_of       :login,    :with => Authentication.login_regex, :message => Authentication.bad_login_message
 
-  validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
-  validates_length_of       :name,     :maximum => 100
+  #validates_format_of       :name,     :with => Authentication.name_regex,  :message => Authentication.bad_name_message, :allow_nil => true
+  #validates_length_of       :name,     :maximum => 100
 
   validates_presence_of     :email
   validates_inclusion_of    :agree, :in => [true] ,:message => "You should agree to the terms"
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110127072925) do
+ActiveRecord::Schema.define(:version => 20110131133439) do
 
   create_table "answers", :force => true do |t|
     t.integer  "candidate_id"
