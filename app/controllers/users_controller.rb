58b89867ea134bab058c3ee99b5f40cf1f286b1d
unknown <SaloO@.(none)>
2011-02-02 16:47:05 +0500

    nothing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -278,7 +278,7 @@ class UsersController < ApplicationController
     if @usr_pckg.package_id!=0
       @pckg = Chargify::Subscription.find_by_customer_reference(current_user.id)
 
-      @date = @pckg.current_period_ends_at.to_date - Time.new.to_date
+      @date = (@pckg.current_period_ends_at.to_date - Time.new.to_date).to_natural_language 
     end
     render :layout=>false
 
