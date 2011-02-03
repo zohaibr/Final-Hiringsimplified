03cefded417ba29fa747d50e39fb25000c95e487
unknown <SaloO@.(none)>
2011-02-03 15:31:05 +0500

    nothing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -272,7 +272,7 @@ class UsersController < ApplicationController
     end
 
 
-    @pckgx = Chargify::Subscription.find(400194)
+    #@pckgx = Chargify::Subscription.find(400194)
     #@sub = Chargify::Subscription.find_by_customer_reference(current_user.id)
 
     @usr_pckg = UserPackage.find_by_user_id current_user.id
@@ -355,7 +355,7 @@ class UsersController < ApplicationController
        # Rails.logger.debug("SUB ID: #{pckg}")
         usr_pckg = UserPackage.find_by_package_id pckg.product.id
 
-        user = User.find(pckg.reference)
+        user = User.find(pckg.customer.reference)
 
         if pckg.state != 'active'
           Notifier.deliver_trigger_subscription(user.email,'Your subscription has been canceled')
