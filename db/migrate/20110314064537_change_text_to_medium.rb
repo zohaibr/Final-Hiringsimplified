4a7525afbd898db0e863a94d84c898aaa8bdb87f
unknown <SaloO@.(none)>
2011-03-15 20:57:47 +0500

    n

--- db/migrate/20110314064537_change_text_to_medium.rb
+++ db/migrate/20110314064537_change_text_to_medium.rb
@@ -1,9 +1,9 @@
 class ChangeTextToMedium < ActiveRecord::Migration
   def self.up
-    change_column :answers, :notepad, :mediumtext
+   
   end
 
   def self.down
-    change_column :answers, :notepad, :text
+    
   end
 end
